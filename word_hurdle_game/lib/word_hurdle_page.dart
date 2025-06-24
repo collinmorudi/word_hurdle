cb93e5b36d79e0a442b40bf6e8025d0eae67f4b4
Collin Morudi <collinmorudi@gmail.com>
2025-06-24 12:57:12 +0200

    Feat: Added the boarder using GridView to the Word Hurdle Page

--- word_hurdle_game/lib/word_hurdle_page.dart
+++ word_hurdle_game/lib/word_hurdle_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:word_hurdle_game/hurdle_provider.dart';
+import 'package:word_hurdle_game/wordle_view.dart';
 
 class WordHurdlePage extends StatefulWidget {
   const WordHurdlePage({super.key});
@@ -10,7 +11,6 @@ class WordHurdlePage extends StatefulWidget {
 }
 
 class _WordHurdlePageState extends State<WordHurdlePage> {
-
   @override
   void didChangeDependencies() {
     Provider.of<HurdleProvider>(context, listen: false).init();
@@ -20,9 +20,30 @@ class _WordHurdlePageState extends State<WordHurdlePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Word Hurdle"),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Word Hurdle"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Consumer<HurdleProvider>(
+                  builder:(context, provider, child) =>  GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 5,
+                      mainAxisSpacing: 4,
+                      crossAxisSpacing: 4,
+                    ),
+                    itemCount: provider.hurdleBoard.length,
+                    itemBuilder: (context, index) {
+                      final wordle = provider.hurdleBoard[index];
+                      return WordleView(wordle: wordle);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
